22c5b45da1f73b9e206d1a29bb998bbd614a2e61
san-had <hadhazis@gmail.com>
2017-06-05 22:01:31 +0200

    OMSZ small change

---
+++ IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTest
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+        [TestMethod]
+        public void ActualDataCheck_idojaras3File_NormalFile()
+        {
+            // Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
